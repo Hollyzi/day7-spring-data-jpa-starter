99e8d5d50d659fc63af30c05b51d1eba2d832f35
HUANGHO2 <HUANGHO2@OOCL.COM>
2024-11-26 17:40:51 +0800

    test:change test should_update_employee_success to real operation

--- src/test/java/com/oocl/springbootemployee/controller/EmployeeControllerTest.java
+++ src/test/java/com/oocl/springbootemployee/controller/EmployeeControllerTest.java
@@ -165,7 +165,8 @@ class EmployeeControllerTest {
     @Test
     void should_update_employee_success() throws Exception {
         // Given
-        Integer givenId = 1;
+        List<Employee> originEmployees = employeeRepository.findAll();
+        Integer givenId = originEmployees.get(0).getId();
         String givenName = "New Employee";
         Integer givenAge = 30;
         Gender givenGender = Gender.FEMALE;
@@ -191,9 +192,9 @@ class EmployeeControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.age").value(givenAge))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.gender").value(givenGender.name()))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.salary").value(givenSalary));
-        List<Employee> employees = employeeMemoryRepository.findAll();
+        List<Employee> employees = employeeRepository.findAll();
         assertThat(employees).hasSize(5);
-        assertThat(employees.get(0).getId()).isEqualTo(1);
+        assertThat(employees.get(0).getId()).isEqualTo(givenId);
         assertThat(employees.get(0).getName()).isEqualTo(givenName);
         assertThat(employees.get(0).getAge()).isEqualTo(givenAge);
         assertThat(employees.get(0).getGender()).isEqualTo(givenGender);
